b007b323d10243715172a48d0a6c79750fabd700
pull-vert <frederic.montariol@gmail.com>
2019-10-08 12:30:01 +0200

    prepare hashcode, commented now. When available I will think about equals too ;)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kotlin-inline-collections
+
 Multiplatform Kotlin inline classes for collections
+
+## inspirations :
+* https://github.com/eclipse/eclipse-collections/blob/master/eclipse-collections/src/main/java/org/eclipse/collections/impl/list/immutable/ImmutableArrayList.java
+* Kotlin collections
+* Java collections
--- src/jdk8Main/kotlin/com/pullvert/collections/iterable/MultiIntIterable.kt
+++ src/jdk8Main/kotlin/com/pullvert/collections/iterable/MultiIntIterable.kt
@@ -3,6 +3,8 @@ package com.pullvert.collections
 import java.io.Serializable
 import java.util.*
 
+
+
 @Suppress("NON_PUBLIC_PRIMARY_CONSTRUCTOR_OF_INLINE_CLASS")
 actual inline class MultiIntIterable internal actual constructor(internal val values: IntArray) : Iterable<Int>, Serializable {
 
@@ -14,4 +16,9 @@ actual inline class MultiIntIterable internal actual constructor(internal val va
     override fun spliterator(): Spliterator<Int> {
         return Spliterators.spliteratorUnknownSize<Int>(iterator(), 0)
     }
+
+//    fixme : not supported yet in 1.3.50 (nor 1.3.60-eap-22 nor 1.3.70-dev-260)
+//    override fun hashCode(): Int {
+//        return Arrays.hashCode(values)
+//    }
 }
